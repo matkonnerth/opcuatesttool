45f9d468ce9af28b93a100f36fa6e2013e7a4b8b
Matthias Konnerth <matthias.konnerth@engel.at>
2021-05-12 06:16:30 +0200

    add targets

--- hmi/testToolApp/src/app/target.ts
+++ hmi/testToolApp/src/app/target.ts
@@ -13,14 +13,24 @@ export const targets: Target[] =
             port: 4840
         },
         {
+            name: 'sjutImm',
+            host: '10.11.65.196',
+            port: 4840
+        },
+        {
+            name: 'sjutRobot',
+            host: '10.11.65.197',
+            port: 4840
+        },
+        {
             name: 'localhost',
             host: 'localhost',
             port: 4840
         },
         {
-            name: 'unified',
+            name: 'unifiedLocalhost',
             host: 'localhost',
             port: 48010
-        }
+        },
     ];
 
