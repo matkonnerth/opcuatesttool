6453f2776e8723dc72122f3fa70f59b6bff9d46e
Matthias Konnerth <matthias.konnerth@engel.at>
2021-05-17 07:02:34 +0200

    fix environment

--- hmi/testToolApp/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  DefaultLanguage: 'en',
-  production: true,
-  development: false,
-  environmentName: 'PROD',
-  ResultsPerPage:[5,10,25,50,75,100],
-  baseURL: '/'
-};
